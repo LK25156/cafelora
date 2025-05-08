8e09623b33278a473ac530fa31c13ee80a64ec1b
Lenka Koubková <lvseteckova@gmail.com>
2025-05-08 18:29:04 +0200

    úprava Menu.jsx

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,5 +1,5 @@
 import './Menu.css';
-import {Drink} from '../Drink/Drink'
+import Drink from '../Drink/Drink'
 
 export const Menu = ({drinks}) => {
   return (
@@ -23,56 +23,12 @@ export const Menu = ({drinks}) => {
 )}
 </div>  
 
-        <div className="drink">
-          <div className="drink__product">
-            <div className="drink__cup">
-              <img src="/cups/doppio.png" />
-            </div>
-            <div className="drink__info">
-              <h3>Doppio</h3>
-              <div className="layer">
-                <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                <div className="layer__label">espresso</div>
-              </div>
-            </div>
-          </div>
-          <form className="drink__controls">
-            <input type="hidden" className="order-id" value="1" />
-            <button className="order-btn">
-              Objednat
-            </button>
-          </form>
-        </div>
-
-        <div className="drink">
-          <div className="drink__product">
-            <div className="drink__cup">
-              <img src="/cups/lungo.png" />
-            </div>
-            <div className="drink__info">
-              <h3>Lungo</h3>
-              <div className="layer">
-                <div className="layer__color" style={{ backgroundColor: '#b0dee1' }} />
-                <div className="layer__label">voda</div>
-              </div>
-              <div className="layer">
-                <div className="layer__color" style={{ backgroundColor: '#613916' }} />
-                <div className="layer__label">espresso</div>
-              </div>
-            </div>
-          </div>
-          <form className="drink__controls">
-            <input type="hidden" className="order-id" value="2" />
-            <button className="order-btn">
-              Objednat
-            </button>
-          </form>
-        </div>
-      </div>
       <div className="order-detail">
         <a href="/order.html">Detail objednávky</a>
       </div>
+      </div>
   </section>
-  )
-};
+  );
+}
 
+export default Menu;
